dfb3213f5ee60cbd02ac190ac0babbe2fac8d593
AxeliaYeo <yeo_shu_axelia@s2023.ssts.edu.sg>
2024-07-06 11:33:22 +0800

    Initial Commit

---
+++ Boo/BooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooApp.swift
+//  Boo
+//
+//  Created by Axelia Yeo on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
