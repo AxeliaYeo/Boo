87ba3ca20d52d6f39e21a450f652a2101c5be19a
Zicheng <zc123playz@gmail.com>
2024-07-13 11:33:42 +0800

    not a potato

--- Boo/ContentView.swift
+++ Boo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello im a potato!")
+            Text("Hello im not a potato!")
         }
         .padding()
     }
